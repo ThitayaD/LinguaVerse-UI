b41f9bcda67644df275c734b2913387023f6c53d
tebit <tibet.tb@gmail.com>
2023-11-21 16:05:55 +0700

    demo

--- src/pages/Page41.js
+++ src/pages/Page41.js
@@ -16,7 +16,9 @@ const Page41 = () => {
 
   const getVideo = useCallback(async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 390, height: 844, facingMode: "environment" } });
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 390, height: 844, facingMode: "environment" },
+      });
       const video = videoRef.current;
       if (video) {
         video.srcObject = stream;
@@ -34,63 +36,67 @@ const Page41 = () => {
   const takePhoto = () => {
     const width = 390;
     const height = 844;
-  
+
     const video = videoRef.current;
     const photo = photoRef.current;
-  
-    if (!video || !photo) {
-        console.error('Video or photo elements are not available.');
-        return;
-    }
-  
-    photo.width = width;
-    photo.height = height;
-  
-    const ctx = photo.getContext('2d');
-    if (!ctx) {
-        console.error('2D rendering context is not available.');
-        return;
-    }
-  
-    // Flip the canvas horizontally
-    ctx.setTransform(-1, 0, 0, 1, width, 0);
-  
-    // Draw the video frame on the canvas
-    ctx.drawImage(video, 0, 0, width, height);
-  
-    // Reset the transformation matrix
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-  
-    setHasPhoto(true);
-  
-    // Send the photo to the backend immediately after taking it
-    sendPhotoToBackend(photo);
-  };
-  
 
-  const sendPhotoToBackend = (photo) => {
-    if (!photo) {
-      console.error('Photo element is not available.');
+    if (!video || !photo) {
+      console.error("Video or photo elements are not available.");
       return;
     }
 
-    photo.toBlob(blob => {
-        const formData = new FormData();
-        formData.append('file', blob, 'photo.png');
+    photo.width = width;
+    photo.height = height;
 
-        fetch('https://localhost:5000/upload-photo', {
-            method: 'POST',
-            body: formData,
+    const ctx = photo.getContext("2d");
+    if (!ctx) {
+      console.error("2D rendering context is not available.");
+      return;
+    }
+
+    // Draw the video frame on the canvas
+    ctx.drawImage(video, 0, 0, width, height);
+
+    setHasPhoto(true);
+
+    // Send the photo to the backend immediately after taking it
+    sendPhotoToBackend(photo);
+  };
+
+  const sendPhotoToBackend = (photo) => {
+    if (!photo) {
+      console.error("Photo element is not available.");
+      return;
+    }
+
+    photo.toBlob(
+      (blob) => {
+        const formData = new FormData();
+        formData.append("file", blob, "photo.png");
+
+        fetch("http://localhost:9000/upload-photo", {
+          method: "POST",
+          body: formData,
         })
-        .then(response => response.json())
-        .then(data => {
+          .then((response) => response.json())
+          .then((data) => {
             console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }, 'image/png');
-};
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+          fetch('http://localhost:6969/close')
+          .then(response => response.json())
+          .then(data => {
+            console.log("Server response:", data.message);
+          })
+          .catch(err => {
+            console.error("Error sending start request to server:", err);
+          });
+          },
+      "image/png"
+    );
+  };
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -101,43 +107,33 @@ const Page41 = () => {
         image.onload = () => {
           const width = 390;
           const height = 844;
-  
+
           const photo = photoRef.current;
-          if (!photo) {
-            console.error('Photo element is not available.');
+          const video = videoRef.current;
+
+          if (!photo || !video) {
+            console.error("Photo or video element is not available.");
             return;
           }
-  
+
           photo.width = width;
           photo.height = height;
-  
-          const ctx = photo.getContext('2d');
+
+          const ctx = photo.getContext("2d");
           if (!ctx) {
-            console.error('2D rendering context is not available.');
+            console.error("2D rendering context is not available.");
             return;
           }
-  
-              // Flip the canvas horizontally
-          ctx.setTransform(-1, 0, 0, 1, width, 0);
 
-          // Draw the video frame on the canvas
-          ctx.drawImage(video, 0, 0, width, height);
+          ctx.drawImage(image, 0, 0, width, height);
 
-          // Reset the transformation matrix
-          ctx.setTransform(1, 0, 0, 1, 0, 0);
-  
           setHasPhoto(true);
-  
-          // Send the photo to the backend after uploading
-          sendPhotoToBackend(photo);
         };
         image.src = e.target.result;
       };
       reader.readAsDataURL(file);
     }
   };
-  
-  
 
   const goBack = () => {
     setHasPhoto(false);
@@ -152,14 +148,20 @@ const Page41 = () => {
           src="/-icon-arrow-left.svg"
           onClick={onIconArrowLeftClick}
         />
-        <video className="mirror" autoPlay={true} playsInline={true} muted={true} ref={videoRef} ></video>
-        <button className="snap-photo" onClick={takePhoto}>SNAP!</button>
-        <input type="file" accept="image/*" onChange={handleFileUpload} style={{ display: 'none' }} id="file-upload" />
-        <label htmlFor="file-upload" className="custom-file-upload">Upload Photo</label>
+        <video className="mirror" autoPlay={true} playsInline={true} muted={true} ref={videoRef}></video>
+        <button className="snap-photo" onClick={takePhoto}>
+          SNAP!
+        </button>
+        <input type="file" accept="image/*" onChange={handleFileUpload} style={{ display: "none" }} id="file-upload" />
+        <label htmlFor="file-upload" className="custom-file-upload">
+          Upload Photo
+        </label>
       </div>
-      <div className={`result ${hasPhoto ? 'hasPhoto' : ''}`}>
-        <canvas ref={photoRef} ></canvas>
-        <button className='back-button' onClick={goBack}>BACK</button>
+      <div className={`result ${hasPhoto ? "hasPhoto" : ""}`}>
+        <canvas ref={photoRef}></canvas>
+        <button className="back-button" onClick={goBack}>
+          BACK
+        </button>
       </div>
     </div>
   );
